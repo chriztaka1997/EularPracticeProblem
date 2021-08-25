916f8565586885f0d3181d2b990f8ce83b74cde7
Christopher Imantaka <30333920+chriztaka1997@users.noreply.github.com>
2021-08-24 19:51:48 -0700

    Problem 3 fixed

--- problem3.py
+++ problem3.py
@@ -6,8 +6,16 @@
 #
 #####################################
 def problem3(num):
+    def is_prime(num):
+        if num > 1:
+            for i in range(2, num):
+                if num % i == 0:
+                    return False
+            return True
+        else:
+            return False
     largest = 0
     for i in range(1, int(num**0.5)):
         if num % i == 0:
-            largest = i
+            if is_prime(i): largest = i
     return largest
