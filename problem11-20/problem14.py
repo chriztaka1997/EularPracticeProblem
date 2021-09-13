370a97d8f5d4dfc4a7dad7f47c95f5469eb9fb82
Christopher Imantaka <30333920+chriztaka1997@users.noreply.github.com>
2021-09-12 18:05:15 -0700

    problem 13 done. Added TODO: on problem 14

---
+++ problem11-20/problem14.py
@@ -0,0 +1,27 @@
+################################
+#
+# Longest Collatz sequence
+#
+################################
+def problem14():
+
+    def collatz(num):
+        copy_num = num
+        list_of_collatz = [num]
+        while copy_num != 1:
+            if copy_num % 2 == 0:
+                copy_num /= 2
+            else:
+                copy_num = 3 * copy_num + 1
+            
+            list_of_collatz.append(copy_num)
+
+        return len(list_of_collatz)
+
+    max_num_sequence = 0
+    for number in range(1000000):
+        max_num_sequence = max(max_num_sequence, collatz(number))
+    
+    return max_num_sequence
+
+print(problem14())
