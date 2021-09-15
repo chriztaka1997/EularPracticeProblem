c09f8cacb566078fdc17df8b8dd3487e6efbe18d
Christopher Imantaka <30333920+chriztaka1997@users.noreply.github.com>
2021-09-14 19:15:06 -0700

    Problem 16 done

--- problem11-20/problem16.py
+++ problem11-20/problem16.py
@@ -3,3 +3,9 @@
 # Power digit sum
 #
 ###############################
+def problem16():
+    value_int = 2**1000
+    list_int = [int(digit) for digit in str(value_int)]
+    return sum(list_int)
+
+print(problem16())
