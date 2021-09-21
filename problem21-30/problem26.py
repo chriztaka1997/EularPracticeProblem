0687de653c5fc8f0055a1e2b76c69eb7f00b207f
Christopher Imantaka <30333920+chriztaka1997@users.noreply.github.com>
2021-09-20 21:59:15 -0700

    problem 26 done

--- problem21-30/problem26.py
+++ problem21-30/problem26.py
@@ -3,3 +3,23 @@
 # Reciprocal cycles
 #
 #######################
+def problem26(denum):
+    
+    def reciprocal_cycles(num):
+
+        seen = {}
+        x = 1
+        while (x % num not in seen) and (x % num != 0):
+            seen[x % num] = True
+            x = x * 10
+
+        return len(seen)
+
+    num_fraction_list = []
+    for i in range(1, denum):
+        num_fraction_list.append(reciprocal_cycles(i))
+
+    return num_fraction_list.index(max(num_fraction_list)) + 1
+
+
+print(problem26(1000))
